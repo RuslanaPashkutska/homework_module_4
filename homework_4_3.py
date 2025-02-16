e058d3b29a58b709887086c3eb4596b88759cced
RuslanaPashkutska <ryslana.pashkytska@gmail.com>
2025-02-16 23:35:21 +0100

    update
    finish the task

--- homework_4_3.py
+++ homework_4_3.py
@@ -1,28 +1,28 @@
-from colorama import Fore
+from colorama import Fore, Style
 import sys
-
-path = sys.argv[1]
 from pathlib import Path
-
-parent_folder_path = Path(path)
 
 
 def parse_folder(path):
     if not path.exists():
         print(Fore.RED + f"Error: La ruta '{path}' no existe." + Style.RESET_ALL)
         return
-
+    if not path.is_dir():
+        print(Fore.RED + f"Error: La ruta '{path}' no es un directorio." + Style.RESET_ALL)
+        return
     for element in path.iterdir():
         if element.is_dir():
-            print(Fore.RED + f"Parse folder: This is folder - {element.name}")
+            print(Fore.RED + f"Parse folder: This is folder - {element.name}" + Style.RESET_ALL)
             parse_folder(element)
         if element.is_file():
-            print(Fore.GREEN + f"Parse folder: This is file - {element.name}")
+            print(Fore.GREEN + f"Parse folder: This is file - {element.name}" + Style.RESET_ALL)
 
 
-if len(sys.argv) < 2:
-    print(Fore.YELLOW + "Uso: python homework_4.3.py /Users/ruslana/Desktop/my_repo1/Modul_4")
-    sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(Fore.YELLOW + "Uso: python homework_4_3.py /Users/ruslana/Desktop/homework_module_4")
+        sys.exit(1)
 
 parent_folder_path = Path(sys.argv[1])
 parse_folder(parent_folder_path)
